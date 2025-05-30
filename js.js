8379cf06e9761d9e4934541ef3027a2443d98108
MasterSheep <risithcha@gmail.com>
2025-05-29 23:20:18 -0700

    Cut Sheet Proof

--- js.js
+++ js.js
@@ -1,12 +1,76 @@
-alert("Testing");
+var FRAMES = [
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}, 
+    {},
+    {},
+    {}
+];
 
-let BTN = document.getElementById("myButton");
-let doSomething = function(event){
-    alert("Sheep");
-}
+var HTML = document.querySelector(".card");
 
-for(var i = 0; i < 3; i++){
-    let newBTN = BTN.cloneNode(true);
-    document.body.append(newBTN);
-    newBTN.addEventListener("click", doSomething);
+var doSomething = function(event, data){
+    console.log(event);
+    console.log(data);
+    alert("FRame  up before you go go");
+};
+
+for(var count = 0; count < FRAMES.length; count++ )
+{
+    var d = FRAMES[count];
+    var copy = HTML.cloneNode(true);
+    var x = (count % 13) + 1;
+    var y = Math.floor(count/13) + 1;
+    copy.style.backgroundPositionX = (100 * (13 - (x - 1))) + "%";
+    copy.style.backgroundPositionY = (100 * ((11 % y) + 1)) + "%";
+    d.suit = Math.floor(count/13);
+    d.number = count % 13; 
+    copy.addEventListener("click", (e)=>{ doSomething(e,d) } );
+    document.body.append(copy);
 }
